c129bb07efbf012a8ea5f777dbd0653b107fb31b
Matheus Lopes <matheus@esusu.org>
2023-02-02 18:49:13 -0300

    feat: add playwright to initialize the test setup

---
+++ tests/components/button/Button.spec.tsx
@@ -0,0 +1,50 @@
+import { test, expect } from "@playwright/experimental-ct-react";
+import { Button } from "../../../src/components";
+import { ThemeProvider } from "../../../src/layout";
+
+test.use({
+  headless: false,
+});
+
+test.describe("Button component test cases.", () => {
+  test("Button should respect the layout", async ({ mount }) => {
+    const button = await mount(<Button>Hello World!</Button>);
+    await expect(button).toHaveScreenshot();
+  });
+
+  test("Button should respect the layout of theme", async ({ mount }) => {
+    const button = await mount(
+      <ThemeProvider>
+        <Button>Hello World!</Button>
+      </ThemeProvider>
+    );
+    await expect(button).toHaveScreenshot();
+  });
+
+  test("Should render with a contained variant", async ({ mount }) => {
+    const button = await mount(
+      <ThemeProvider>
+        <Button variant="contained">Hello World!</Button>
+      </ThemeProvider>
+    );
+    await expect(button).toHaveScreenshot();
+  });
+
+  test("Button should render the correct children text", async ({ mount }) => {
+    const button = await mount(<Button>Hello World!</Button>);
+    await expect(button).toContainText(/Hello world!/i);
+  });
+
+  test("Button should execute the callback function", async ({ mount }) => {
+    let executed: boolean = false;
+    const button = await mount(
+      <Button variant="contained" onClick={() => (executed = true)}>
+        Hello World!
+      </Button>
+    );
+    await button.click({
+      delay: 500,
+    });
+    expect(executed).toBe(true);
+  });
+});
